4bdb79bc44224616129d1bde1918da7ddd853870
Harman Singh <karan.23singh@gmail.com>
2018-02-14 13:00:34 +0530

    fix

--- src/app/shared/api-links.ts
+++ src/app/shared/api-links.ts
@@ -3,7 +3,7 @@ export class ApiLinks {
   //public static flightDetailsBase = 'http://10.177.7.81:8083/flight/detail';
   //public static flightCreateBooking = 'http://10.177.7.81:8083/createBooking';
   //public static makePaymentUrl = 'http://10.177.7.81:8083/payment/card/makePayment';
-  //public static bookingHistoryBaseUrl = 'http://10.177.7.81:8083/booking/superPnr';
+  public static bookingHistoryBaseUrl = 'http://10.177.7.81:8083/booking/superPnr';
 
 
   public static searchBase = 'http://localhost:8083/flight/search';
